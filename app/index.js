f7d8f5cbce524c5b628cbb6cb5a8f063eaa2dc26
Mouss <104678778+MoussCarter@users.noreply.github.com>
2023-03-19 00:31:13 +0100

    initial commit/layout & index

---
+++ app/index.js
@@ -0,0 +1,19 @@
+import { View, Text } from "react-native";
+
+const Home = () => {
+  return (
+    <View>
+      <Text>Home</Text>
+      <Text>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Quae ratione
+        aliquid nesciunt quod ducimus nemo, dignissimos sit, libero perspiciatis
+        ipsum eum tempore asperiores officiis. Ex sequi accusantium id harum
+        dolores, fugit in repudiandae! Debitis quod iusto officia temporibus
+        quasi ex suscipit sequi esse beatae harum corrupti et dicta, magnam
+        earum.
+      </Text>
+    </View>
+  );
+};
+
+export default Home;
